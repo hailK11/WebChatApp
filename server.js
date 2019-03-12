0054038922c8c3835c56cc15e9c4d7fced2c0ca8
hailK11 <akhilkrishna.r2016@vitstudent.ac.in>
2019-03-12 06:06:31 +0530

    Fix#2 jquery bootstrap

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/js', express.static('/node_modules/bootstrap/dist/js'));
-app.use('/js', express.static('/node_modules/jquery/dist'));
 app.use('/css', express.static('/node_modules/bootstrap/dist/css'));
 
 const port = process.env.PORT || 3000
